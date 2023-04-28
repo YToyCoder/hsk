4ad8dc8f0cebbe3f981996e945c40498e9b3c03a
forsilence <3312034934@qq.com>
2023-04-28 22:55:30 +0800

    curried function tests

--- test/UnitTest.hs
+++ test/UnitTest.hs
@@ -4,11 +4,13 @@ import System.Exit
 
 import Test.HUnit
 import LiterateTest
+import HskStart
 
-test1 = TestCase (assertEqual "for add 2 3" 5 (add 2 3))
-
+testAdd = TestCase (assertEqual "for add 2 3" 5 (add 2 3))
+testUncurry = TestCase (assertEqual "curry function " 1 (uncurriedFn (3, 2)))
+testCurry = TestCase (assertEqual "curry function" 1 (curriedFn 3 2))
 main = do 
-  a <- runTestTT (test [test1]) 
-  if ((errors a) + (failures a)== 0) 
-        then exitSuccess
-        else exitFailure
+  a <- runTestTT (test [testAdd, testUncurry, testCurry]) 
+  if errors a + failures a == 0
+    then exitSuccess
+    else exitFailure
