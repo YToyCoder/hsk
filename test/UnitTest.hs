bdbe1feb88e418002ae895182707aa51347b8d34
forsilence <3312034934@qq.com>
2023-04-29 10:36:00 +0800

    types

--- test/UnitTest.hs
+++ test/UnitTest.hs
@@ -3,12 +3,21 @@ module Main where
 import System.Exit
 
 import Test.HUnit
-import LiterateTest
-import HskStart
+    ( assertEqual,
+      runTestTT,
+      Counts(errors, failures),
+      Test(TestCase),
+      Testable(test) )
+import LiterateTest ( add )
+import HskStart ( curriedFn, uncurriedFn )
 
+testAdd :: Test
 testAdd = TestCase (assertEqual "for add 2 3" 5 (add 2 3))
+testUncurry :: Test
 testUncurry = TestCase (assertEqual "curry function " 1 (uncurriedFn (3, 2)))
+testCurry :: Test
 testCurry = TestCase (assertEqual "curry function" 1 (curriedFn 3 2))
+main :: IO b
 main = do 
   a <- runTestTT (test [testAdd, testUncurry, testCurry]) 
   if errors a + failures a == 0
