d65c16f9394dafb455966651d55ed75169739a3f
YToyCoder <3312034934@qq.com>
2023-11-19 13:19:26 +0800

    haskell function

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,17 @@ module Main where
 import LiterateTest (add)
 import Text.Printf (printf)
 
+{- McCarthy 91 function -}
+mc n  | n > 100 = n - 10
+      | otherwise = mc (mc (n + 11))
+
+mc' n | n > 100 = n - 10
+      | otherwise = 91
+
 main =
-  printf "Hello %d\n"  i where 
-    i = add 3 4
+  mapM_ print cmp_result
+    where 
+      source = [10 .. 200]
+      maped = map mc source 
+      mc_fixed = map mc' source
+      cmp_result = zipWith (==) mc_fixed maped -- map is_equal (zip mc_fixed maped)
